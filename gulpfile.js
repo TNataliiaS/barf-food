cebcfc3dc24791c4ba9550dae35341b863d6bcb3
Nataliia <kurinak@inbox.ru>
2022-09-11 18:06:49 +0400

    add faq and footer blocks

--- gulpfile.js
+++ gulpfile.js
@@ -100,7 +100,7 @@ function css() {
             cascade: true
         }))
         .pipe(gcmq())
-        // .pipe(gulp.dest(path.build.css))
+        .pipe(gulp.dest(path.build.css))
         .pipe(cleanCSS({
             level: {
                 1: {
