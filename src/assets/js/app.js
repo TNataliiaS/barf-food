9723c38ab1dfbd0a05f183ff8953923101429383
Nataliia <kurinak@inbox.ru>
2022-10-23 17:51:06 +0400

    cross-browser compatibility

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,10 @@
 'use strict';
+window.addEventListener('DOMContentLoaded', pageInteractive);
+window.addEventListener('resize', pageInteractive);
 
-// Smooth scoll element
-// ****************
-document.addEventListener('DOMContentLoaded', function () {
+function pageInteractive () {
+    // Smooth scoll element
+    // ****************
     const links = document.querySelectorAll('.site-nav__link');
 
     for(let i = 0; i < links.length; i++) {
@@ -23,128 +25,128 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     }
-});
 
 
-// Menu Toggle
-// ****************
-const burgerToggle = document.getElementById('burger');
-const siteNavShow = document.getElementById('site-nav');
-const bodyDisableScroll = document.querySelector('body');
+    // Menu Toggle
+    // ****************
+    const burgerToggle = document.getElementById('burger');
+    const siteNavShow = document.getElementById('site-nav');
+    const bodyDisableScroll = document.querySelector('body');
 
-burgerToggle.addEventListener('click', () => {
-    burgerToggle.classList.toggle('burger--active');
-    siteNavShow.classList.toggle('site-nav--show');
-    bodyDisableScroll.classList.toggle('no-scroll');
-});
+    burgerToggle.addEventListener('click', () => {
+        burgerToggle.classList.toggle('burger--active');
+        siteNavShow.classList.toggle('site-nav--show');
+        bodyDisableScroll.classList.toggle('no-scroll');
+    });
 
-document.querySelectorAll('.site-nav__link').forEach(e => e.addEventListener('click', () => {
-    burgerToggle.classList.remove('burger--active');
-    siteNavShow.classList.remove('site-nav--show');
-    bodyDisableScroll.classList.remove('no-scroll');
-}));
-
-
-// Fixed header on scroll
-// ****************
-const firstEl = document.getElementById('intro');
-const header = document.getElementById('header');
-const headerHeight = header.offsetHeight;
-const firstElHeight = firstEl.offsetHeight;
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= firstElHeight - headerHeight) {
-        header.classList.add('header--fixed');
-    } else {
-        header.classList.remove('header--fixed');
-    }
-});
+    document.querySelectorAll('.site-nav__link').forEach(e => e.addEventListener('click', () => {
+        burgerToggle.classList.remove('burger--active');
+        siteNavShow.classList.remove('site-nav--show');
+        bodyDisableScroll.classList.remove('no-scroll');
+    }));
 
 
-// Info Slider (swiper js https://swiperjs.com/ )
-// ****************
-const infoSlider = new Swiper('.info-slider', {
-	pagination: {
-		el: '.info-slider__pagination',
-		clickable: true,
-	},
+    // Fixed header on scroll
+    // ****************
+    const firstEl = document.getElementById('intro');
+    const header = document.getElementById('header');
+    const headerHeight = header.offsetHeight;
+    const firstElHeight = firstEl.offsetHeight;
 
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-		},
-
-		768: {
-			slidesPerView: 2,
-		},
-	},
-	autoHeight: true,
-	slidesPerView: 2,
-});
-
-
-// Sets/Rations Slider (swiper js https://swiperjs.com/ )
-// ****************
-const swiper = new Swiper('.swiper--primary', {
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-        pageUpDown: true
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-    },
-	autoHeight: true,
-	spaceBetween: 24,
-	slidesPerView: 'auto',
-});
-
-
-// Norms Slider (swiper js https://swiperjs.com/ )
-// ****************
-const normsSlider = new Swiper('.norms__slider-swiper', {
-    pagination: {
-        el: '.norms__slider-pagination',
-        clickable: true,
-    },
-    breakpoints: {
-		320: {
-			slidesPerView: 1,
-            pagination: {
-                el: '.norms__slider-pagination-mobile',
-                clickable: true,
-                renderBullet: function (index, className) {
-                    let labels = ['Для взрослых', 'Для щенков'];
-                    return '<div class="' + className + '">' + (labels[index]) + '</div>';
-                }
-            },
-		},
-
-		376: {
-			slidesPerView: 2,
-            slidesPerColumn: 2,
-		},
-
-        576: {
-			slidesPerView: 'auto',
-		},
-	},
-	slidesPerView: 2,
-});
-
-
-// Accordion faq
-// ****************
-const accItems = document.querySelectorAll('.faq__item');
-
-accItems.forEach((acc) => acc.addEventListener('click',
-    function toggleAcc() {
-        accItems.forEach((item) => item != this ? item.classList.remove('faq__item--active') : null);
-
-        if(this.classList != 'faq__item--active') {
-            this.classList.toggle('faq__item--active');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY >= firstElHeight - headerHeight) {
+            header.classList.add('header--fixed');
+        } else {
+            header.classList.remove('header--fixed');
         }
-    }
-));
+    });
+
+
+    // Info Slider (swiper js https://swiperjs.com/ )
+    // ****************
+    const infoSlider = new Swiper('.info-slider', {
+        pagination: {
+            el: '.info-slider__pagination',
+            clickable: true,
+        },
+
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+
+            768: {
+                slidesPerView: 2,
+            },
+        },
+        autoHeight: true,
+        slidesPerView: 2,
+    });
+
+
+    // Sets/Rations Slider (swiper js https://swiperjs.com/ )
+    // ****************
+    const swiper = new Swiper('.swiper--primary', {
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+            pageUpDown: true
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        autoHeight: true,
+        spaceBetween: 24,
+        slidesPerView: 'auto',
+    });
+
+
+    // Norms Slider (swiper js https://swiperjs.com/ )
+    // ****************
+    const normsSlider = new Swiper('.norms__slider-swiper', {
+        pagination: {
+            el: '.norms__slider-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                pagination: {
+                    el: '.norms__slider-pagination-mobile',
+                    clickable: true,
+                    renderBullet: function (index, className) {
+                        let labels = ['Для взрослых', 'Для щенков'];
+                        return '<div class="' + className + '">' + (labels[index]) + '</div>';
+                    }
+                },
+            },
+
+            376: {
+                slidesPerView: 2,
+                slidesPerColumn: 2,
+            },
+
+            576: {
+                slidesPerView: 'auto',
+            },
+        },
+        slidesPerView: 2,
+    });
+
+
+    // Accordion faq
+    // ****************
+    const accItems = document.querySelectorAll('.faq__item');
+
+    accItems.forEach((acc) => acc.addEventListener('click',
+        function toggleAcc() {
+            accItems.forEach((item) => item != this ? item.classList.remove('faq__item--active') : null);
+
+            if(this.classList != 'faq__item--active') {
+                this.classList.toggle('faq__item--active');
+            }
+        }
+    ));
+}
